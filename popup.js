309d95f6bca3b17d6593773f4e27aa033709a941
Franco Cravero <francordie@gmail.com>
2019-07-07 16:13:16 -0400

    Send current tab URL to Bicineta /noticias

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('change-color');
+let addLink = document.getElementById('add-link')
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -11,6 +12,25 @@ changeColor.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '"; console.log("green-ed")'});
+        {
+					code: 'document.body.style.backgroundColor = "' + color + '";' +
+					'msg = "Current tab title changed from [" + document.title;' +
+					'document.title = "Bicineta";' +
+					'alert(msg + "] to [" + document.title + "]");'
+				});
   });
 };
+
+addLink.onclick = function(element) {
+	addLink.innertHTML = 'Sending ...';
+	extensionId = chrome.runtime.id;
+	params = 'extensionId=' + extensionId + '&url=';
+	redirectUrl = 'https://www.bicineta.cl/noticias/ingresar?' + params;
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.executeScript(
+    tabs[0].id,
+    {
+			code: 'alert("Sending link to Bicineta"); location.href="' + redirectUrl + tabs[0].url + '";'
+		});
+  });
+}
